54e33703be3f49aeeac643b94b5fbe9d8743d422
jdonahoe8 <jdonahoe8@gatech.edu>
2023-11-22 19:01:48 -0500

    Initial GPT Template:

--- pantrypal/recipes/views.py
+++ pantrypal/recipes/views.py
@@ -25,6 +25,27 @@ class DeleteRecipeView(TemplateView):
 class AddRecipeView(TemplateView):
     
     def get(self, request):
+        alignmentPrompt = """
+        You are a food recipe generator for an app called 'pantrypal' to create delicious ideas for meals.
+        You will be given a list of ingredients that a user has, and you should ONLY use these ingredients.
+        The recipes should be relatively simple and interesting to make.
+        
+        You only give the recipes in the following format, adjusting the number of steps as necessary:
+        Hi User,
+        The name of the recipe is: {insert_recipe_name_here}
+        Step 1: {insert_step_1_step}
+        Step 2: {insert_step_2_step}
+        Step 3: {insert_step_3_step}
+        Step 4: {insert_step_4_step}
+        Step 5: {insert_step_5_step}
+        Step 6: {insert_step_6_step}
+        Step 7: {insert_step_7_step}
+        Final Step: Enjoy!
+        """
+
+
+
+
         # GPT HERE
         new_recipe = Recipe(pal=request.user.pal, name='name from GPT', steps='Steps from GPT')
         new_recipe.save()
