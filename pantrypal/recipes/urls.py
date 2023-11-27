a80a5f690137503325c450b4b0eeb2875cbb361e
garrettmeek <me@garrettmeek.com>
2023-11-27 12:14:32 -0500

    Added button to post a recipe

--- pantrypal/recipes/urls.py
+++ pantrypal/recipes/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.RecipeView.as_view(), name='recipes'),
     path('add_recipe/', views.AddRecipeView.as_view(), name='add_recipe'),
     path('delete_recipe/<int:pk>', views.DeleteRecipeView.as_view(), name='delete_recipe'),
+    path('post/<int:pk>', views.PostView.as_view(), name='post'),
     
 ]
